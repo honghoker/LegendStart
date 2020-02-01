7daf60205c26500a59b87797695b1034185c9b8c
honghoker <ekflzhftk@naver.com>
2020-02-02 00:14:44 +0900

    20-02-01 HashTag#2
    HashTag#2

--- Hep/ls_listsave/app/src/main/java/com/example/ls_listsave/MainActivity.java
+++ Hep/ls_listsave/app/src/main/java/com/example/ls_listsave/MainActivity.java
@@ -5,73 +5,33 @@ import java.util.List;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.Selection;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
 
-public class MainActivity extends Activity implements
-        HashTagHelper.OnHashTagClickListener {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-
-    private HashTagHelper mTextHashTagHelper;
-    private HashTagHelper mEditTextHashTagHelper;
-
-    private TextView mEditTextView;
-
-    private Toast mToast;
-
+public class MainActivity extends Activity {
+    ArrayList<String> Hashtagar = new ArrayList<String>();
     private List<String> list;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listinit();
         init();
-        listsetting();
     }
 
-    public void init() {
+    public void listinit() {
         list = new ArrayList<String>();
-
-        mEditTextView = (EditText) findViewById(R.id.edit_text_field);
-        final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.edit_text_field);
-
-        autoCompleteTextView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, list)); // 글자 자동완성
-
-        char[] additionalSymbols = new char[]{'_', '$'};
-        // If you set additional symbols not only letters and digits will be a
-        // valid symbols for hashtag
-        // Example:
-        // "hash_tag_with_underscore_and$dolar$sign$is$also$valid_hashtag"
-
-        mTextHashTagHelper = HashTagHelper.Creator.create(getResources()
-                .getColor(R.color.colorPrimary), this, additionalSymbols);
-
-        //mTextHashTagHelper.handle(mHashTagText);
-
-        // Here we don't specify additionalSymbols. It means that in EditText
-        // only letters and digits will be valid symbols
-        mEditTextHashTagHelper = HashTagHelper.Creator.create(getResources()
-                .getColor(R.color.colorPrimaryDark), null);
-        mEditTextHashTagHelper.handle(mEditTextView);
-    }
-
-    public void listsetting() {
         list.add("소고기");
         list.add("돼지고기");
         list.add("오리고기");
@@ -80,45 +40,64 @@ public class MainActivity extends Activity implements
         list.add("개고기");
     }
 
-    @Override
-    public void onHashTagClicked(String hashTag) {
-        Log.v(TAG, "onHashTagClicked [" + hashTag + "]");
-        if (mToast != null) {
-            mToast.cancel();
-        }
-        mToast = Toast.makeText(MainActivity.this, hashTag,
-                Toast.LENGTH_SHORT);
-        mToast.show();
-    }
+    public void init() {
+        final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.clearable_edit);
 
-    ArrayList<String> Hashtagar = new ArrayList<String>();
+        autoCompleteTextView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, list)); // 글자 자동완성
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                hashtag_Add(((TextView)view).getText().toString());
+            }
+        });
+    }
 
     public void onButtonHashTagAddClicked(View v) {
-        if (mEditTextView.getText().length() != 0) {
-            if (Hashtagar.contains(mEditTextView.getText().toString())) {
-                Toast.makeText(getApplicationContext(), "이미 추가한 태그입니다.", Toast.LENGTH_SHORT).show();
-            } else {
-                TextView text = new TextView(this);
-                text.setText(mEditTextView.getText());
-                text.setTextColor(Color.parseColor("#000000"));
-                text.setBackgroundResource(R.drawable.hashtag);
-                        /*LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                        params.setMargins(5,5,10,5);  // 왼쪽, 위, 오른쪽, 아래 순서
-                        text.setLayoutParams(params);*/
+        hashtag_Add(((AutoCompleteTextView)findViewById(R.id.clearable_edit)).getText().toString());
+    }
 
-                FlowLayout.LayoutParams paramss = new FlowLayout.LayoutParams(20, 20);
-                FlowLayout layout = findViewById(R.id.flowlayout);
-                text.setLayoutParams(paramss);
-                layout.addView(text);
 
-                mEditTextView.setText("");
-                //((LinearLayout) findViewById(R.id.HashTagLayout)).addView(text);
-                Hashtagar.add(text.getText().toString());
-            }
+    public void hashtag_Add(String Hash){
+        AutoCompleteTextView HashText = findViewById(R.id.clearable_edit);
+
+        if(HashText.getText().toString().trim().equals("")){
+            Toast.makeText(getApplicationContext(), "내용을 입력해주세요.", Toast.LENGTH_SHORT).show();
         }
-        else{
-            Toast.makeText(getApplicationContext(), "추가할 내용을 입력해주세요.", Toast.LENGTH_SHORT).show();
+        else if(Hashtagar.contains(Hash)){
+            Toast.makeText(getApplicationContext(), "이미 추가한 태그입니다.", Toast.LENGTH_SHORT).show();
+            HashText.setText(HashText.getText().toString().trim());
+        }
+        else if(Hashtagar.size() == 5){
+            Toast.makeText(getApplicationContext(), "태그는 5개까지 추가할 수 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            TextView text = new TextView(this);
+            text.setText("#"+Hash);
+
+            Drawable x = getResources().getDrawable(R.drawable.x);
+            x.setBounds(0, 0, 40, 40);
+            text.setCompoundDrawables(null, null, x, null);
+
+            text.setTextColor(Color.parseColor("#3F729B")); // 인스타 태그 컬러
+            text.setBackgroundResource(R.drawable.hashtag); // 라운드 테두리
+            text.setId(Hashtagar.size());
+
+            FlowLayout.LayoutParams params = new FlowLayout.LayoutParams(20, 20);
+
+
+            text.setLayoutParams(params);
+            ((FlowLayout)findViewById(R.id.flowlayout)).addView(text);
+
+            HashText.setText("");
+            Hashtagar.add(Hash);
         }
     }
 
+    public void onButtondeleteclicked(View v){
+        if(!Hashtagar.isEmpty()) {
+            ((FlowLayout) findViewById(R.id.flowlayout)).removeView(findViewById(Hashtagar.size() - 1));
+            Hashtagar.remove(Hashtagar.size() - 1);
+        }
+    }
 }
