48f056049a3dac6a73a6d77b68e05a0a8245ec99
eungchun <leedool3003@naver.com>
2020-02-01 18:37:35 +0900

    20200201_sunghun
    20200201_sunghun

--- LSKMS/20200201 MyFragment1/app/src/main/java/com/example/myfragment1/MainActivity.java
+++ LSKMS/20200201 MyFragment1/app/src/main/java/com/example/myfragment1/MainActivity.java
@@ -12,11 +12,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+//    private FragmentManager fragmentManager;
+    private Fragment fc, fd;
+
     //프래그먼트는  xml레이아웃 파일 하나랑 자바소스 파일 하나로 정의할 수 있다.
     //이게 하나의 뷰처럼 쓸 수 있는데 뷰하고 약간 다른특성들이 있다.
     //엑티비티를 본떠 만들었기 떄문에 프래그먼트 매니저가 소스코드에서 담당한다.
@@ -27,72 +42,12 @@ public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
     private Fragment fa, fb = null;
 
-
-    //상단 메뉴 달기
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) { //메뉴
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.eventmenu, menu);
-
-        //상단 메뉴 java에서 변경 https://www.it-swarm.dev/ko/android/android%EC%97%90%EC%84%9C-%EB%A9%94%EB%89%B4-%ED%95%AD%EB%A
-        // A%A9%EC%9D%98-%ED%85%8D%EC%8A%A4%ED%8A%B8-%EC%83%89%EC%83%81%EC%9D%84-%EB%B3%80%EA%B2%BD%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95%EC%9D%80-%EB%AC%B4%EC%97%87%EC%9E%85%EB%8B%88%EA%B9%8C/969218497/
-
-        /*
-        int positionOfMenuItem = 2; // 1번 인덱스 메뉴아이템
-
-        MenuItem item = menu.getItem(positionOfMenuItem);
-
-        SpannableString s = new SpannableString("My red MenuItem");
-        s.setSpan(new ForegroundColorSpan(Color.RED), 0, s.length(), 0);
-        item.setTitle(s);
-
-        menu.findItem(R.id.minus).setIcon(R.drawable.ic_map);
-        */
-
+        inflater.inflate(R.menu.menu_list,menu);
         return true;
     }
-
-    public void changeMenu(Menu menu) {
-        if (menuFlag == true) {
-            menu.findItem(R.id.plus).setIcon(R.drawable.ic_map);
-        } else if (menuFlag == false) {
-            menu.findItem(R.id.plus).setIcon(R.drawable.ic_map);
-        }
-    }
-
-    //상단 메뉴 이벤트
-    //액션버튼을 클릭했을때의 동작
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        //or switch문을 이용하면 될듯 하다.
-        if (id == android.R.id.home) {
-            Toast.makeText(this, "홈 클릭", Toast.LENGTH_SHORT).show();
-
-            //Intent homeIntent = new Intent(this, HomeActivity.class);
-            //startActivity(homeIntent);
-        }
-        if (id == R.id.plus) {
-            Toast.makeText(this, "플러스 클릭", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if (id == R.id.minus) {
-            Toast.makeText(this, "마이너스 클릭", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if (id == R.id.ten) {
-            Toast.makeText(this, "10 클릭", Toast.LENGTH_SHORT).show();
-
-            /*
-            Intent settingIntent = new Intent(this, SettingActivity.class);
-            startActivity(settingIntent);
-            */
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,7 +60,18 @@ public class MainActivity extends AppCompatActivity {
         fragment2 = new MenuFragment();
         onFragmentChange(0);
 */
+        toolbar = findViewById(R.id.toolbar);
+//        setActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         //프래그먼트 유지
 
         fragmentManager = getSupportFragmentManager();
@@ -114,13 +80,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         //상단 메뉴
-
-        //액션바 타이틀 변경하기
-        getSupportActionBar().setTitle("App");
-        //액션바 배경색 변경
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
-        //홈버튼 표시
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼 home
+//
+//        //액션바 타이틀 변경하기
+//        getSupportActionBar().setTitle("App");
+//        //액션바 배경색 변경
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
+//        //홈버튼 표시
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼 home
 
 
     }
@@ -176,5 +142,20 @@ public class MainActivity extends AppCompatActivity {
         } else if (index == 1) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment2).commit();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
