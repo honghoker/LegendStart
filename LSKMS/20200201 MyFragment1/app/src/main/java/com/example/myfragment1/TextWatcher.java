52457578facc7a54629dd50105b0fb99fd4d2c68
msk5678 <60131359+msk5678@users.noreply.github.com>
2020-02-09 20:53:07 +0900

    20200209 result X
    ...

---
+++ LSKMS/20200201 MyFragment1/app/src/main/java/com/example/myfragment1/TextWatcher.java
@@ -0,0 +1,32 @@
+/*
+package com.example.myfragment1;
+
+import android.text.Editable;
+
+public class TextWatcher {
+    //선언
+    TextWatcher watcher = new TextWatcher()
+    {
+        @Override
+        public void afterTextChanged(Editable s) {
+            //텍스트 변경 후 발생할 이벤트를 작성.
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+            //텍스트의 길이가 변경되었을 경우 발생할 이벤트를 작성.
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {
+            //텍스트가 변경될때마다 발생할 이벤트를 작성.
+            if (mXMLBuyCount.isFocusable())
+            {
+                //mXMLBuyCount EditText 가 포커스 되어있을 경우에만 실행됩니다.
+            }
+        }
+    }
+}
+*/
