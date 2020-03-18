fa4314d15f81e8fa6589eef8a4ca162fa1327386
honghoker <ekflzhftk@naver.com>
2020-03-18 22:24:27 +0900

    testdelete

--- HERE/20200212_MyFragment1/app/src/main/java/com/example/myfragment1/DataLappingForUnDo/DataLapping_LocationData.java
+++
@@ -1,5 +0,0 @@
-package com.example.myfragment1.DataLappingForUnDo;
-
-public class DataLapping_LocationData {
-
-}
--- HERE/20200212_MyFragment1/app/src/main/java/com/example/myfragment1/LocationList_RecyclerView/SwipeActionInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.myfragment1.LocationList_RecyclerView;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public interface SwipeActionInterface {
-    void onLeftClicked(RecyclerView.ViewHolder viewHolder, int position, RecyclerviewSecondSwipeToDoHelper recyclerviewSecondSwipeToDoHelper);
-    void onRightClicked(RecyclerView.ViewHolder viewHolder, int position, RecyclerviewSecondSwipeDismissHelper recyclerviewSecondSwipeDismissHelper);
-}
